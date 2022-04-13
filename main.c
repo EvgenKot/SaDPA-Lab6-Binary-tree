f7e56a10dc65bd3c61678c89de5927b342468df9
Evgeny Kot <ukal.evgen@gmail.com>
2022-04-14 00:06:38 +0500

    v0.6.1
    Всё ещё не работает удаление

--- main.c
+++ main.c
@@ -20,33 +20,10 @@ typedef struct Node {
 
 
 
-// Минимальный элемент поддерева
-Node *TreeMin(Node *root) {
-    Node *l = root;
-    while (l -> less != NULL)
-        l = l -> less;
-    return l;
-}
-
-// Максимальный элемент поддерева
-Node *TreeMax(Node *root) {
-    Node *r = root;
-    while (r -> more != NULL)
-        r = r -> more;
-    return r;
-}
-
-
-
-
-
 Node *add(Node *root, int key) {
     Node *root2 = root, *root3 = NULL;
     Node *tmp = malloc(sizeof(Node));
-    // Присваивание значения ключу
     tmp->value = key;
-    /* Поиск нужной позиции для вставки (руководствуемся правилом
-    вставки элементов, см. начало статьи, пункт 3) */
     while (root2 != NULL)
     {
         root3 = root2;
@@ -55,14 +32,12 @@ Node *add(Node *root, int key) {
         else
             root2 = root2->more;
     }
-    /* Присваивание указателю на родителя значения указателя root3
-    (указатель root3 был найден выше) */
+    //Присваивание указателю на родителя значения указателя root3
     tmp->parent = root3;
-    // Присваивание указателю на левое и правое поддерево значения NULL
+    //Присваивание указателю на левое и правое поддерево значения NULL
     tmp->less = NULL;
     tmp->more = NULL;
-    /* Вставляем узел в дерево (руководствуемся правилом
-    вставки элементов, см. начало статьи, пункт 3) */
+    //Вставляем узел в дерево 
     if (key < root3->value)
         root3->less = tmp;
     else
@@ -93,67 +68,116 @@ Node *search(Node *root, int data) {
 }
 
 
+// Минимальный элемент поддерева
+Node *TreeMin(Node *root, int *depth) {
+    Node *LessNode = root;
+    while (LessNode -> less != NULL){
+        *depth = *depth + 1;
+        LessNode = LessNode -> less;
+    }
+    return LessNode;
+}
+
+// Максимальный элемент поддерева
+Node *TreeMax(Node *root, int *depth) {
+    Node *MoreNode = root;
+    while (MoreNode -> more != NULL){
+        *depth = *depth + 1;
+        MoreNode = MoreNode -> more;
+    }
+    return MoreNode;
+}
+
 
 Node *succ(Node *root)
 {
-    Node *p = root, *l = NULL;
-// Если есть правое поддерево, то ищем минимальный элемент в этом поддереве
-    if (p -> more != NULL)
-        //return min(p -> right);
-/* Правое дерево пусто, идем по родителям до тех пор,
-пока не найдем родителя, для которого наше поддерево левое */
-    l = p -> parent;
-    while ((l != NULL) && (p == l -> more))
-    {
-        p = l;
-        l = l -> parent;
-    }
-    return l;
+    int MinMoreDepth = 0, MaxLessDepth = 0;
+    Node *MinMoreNode = NULL , *MaxLessNode = NULL;
+//Ищем Минимального среди ветки больших, записываем его глубину
+    MinMoreNode = TreeMin(root->more, &MinMoreDepth);
+//Ищем Максимальное среди ветки меньших, записываем его глубину
+    MaxLessNode = TreeMax(root->less, &MaxLessDepth);
+//Для наибольшей эффективности в качестве искомго узла указываем узел с наибольшей глубиной
+    printf(" %d to left max\n %d to right min\n", MaxLessDepth, MinMoreDepth);
+    if (MinMoreDepth >= MaxLessDepth)
+        return MinMoreNode;
+    else
+        return MaxLessNode;
 }
 
 
 
-Node *delete(Node *root, Node *desired) {
+Node *delete(Node *desired) {
 // Поиск удаляемого узла по ключу
-    Node *p = root, *l = desired, *m = NULL;
-// 1 случай
-    if ((l -> less == NULL) && (l -> more == NULL))
+    Node *TargetNode = desired, *tmp = NULL;
+//узел без дочерних элементов
+    if ((TargetNode -> less == NULL) && (TargetNode -> more == NULL))
     {
-        m = l -> parent;
-        if (l == m -> more) m -> less = NULL;
-        else m -> less = NULL;
-        free(l);
+        tmp = TargetNode -> parent;
+        if (TargetNode == tmp -> more) tmp -> less = NULL;
+        else tmp -> less = NULL;
+        free(TargetNode);
     }
-// 2 случай, 1 вариант - поддерево справа
-    if ((l -> less == NULL) && (l -> more != NULL))
-    {
-        m = l -> parent;
-        if (l == m -> more) m -> more = l -> more;
-        else m -> less = l -> more;
-        free(l);
+//поддерево справа
+    if ((TargetNode -> less == NULL) && (TargetNode -> more != NULL)) {
+        tmp = TargetNode -> parent;
+        if (TargetNode == tmp -> more) tmp -> more = TargetNode -> more;
+        else tmp -> less = TargetNode -> more;
+        free(TargetNode);
     }
-// 2 случай, 2 вариант - поддерево слева
-    if ((l -> less != NULL) && (l -> more == NULL))
-    {
-        m = l -> parent;
-        if (l == m -> more) m -> more = l -> less;
-        else m -> less = l -> more;
-        free(l);
+//поддерево слева
+    if ((TargetNode -> less != NULL) && (TargetNode -> more == NULL)) {
+        tmp = TargetNode -> parent;
+        if (TargetNode == tmp -> more) tmp -> more = TargetNode -> less;
+        else tmp -> less = TargetNode -> more;
+        free(TargetNode);
     }
-// 3 случай
-    if ((l -> less != NULL) && (l -> more != NULL))
-    {
-        /*
-        m = succ(l);
-        l -> key = m -> key;
-        if (m -> right == NULL)
-            m -> parent -> left = NULL;
-        else m -> parent -> left = m -> right;
-        */
-        free(m);
+//2 поддерева
+    if ((TargetNode -> less != NULL) && (TargetNode -> more != NULL)) {
+        tmp = succ(TargetNode); //При одиновковой глубине указывает на большее 'о'
+
+
+    //Если перемещаем из большего поддерева
+        if (tmp -> less == NULL){
+            if (tmp -> parent -> less == tmp)
+                tmp -> parent -> less = NULL;
+            else
+                tmp -> parent -> more = NULL;
+        }
+            
+    //Eсли перемещаем ищ меньшего поддерева
+        else if (tmp -> more == NULL){
+            if (tmp -> parent-> more == tmp)
+                tmp -> parent -> more = NULL;
+            else
+                tmp -> parent -> less = NULL;
+        }
+
+        tmp -> more = TargetNode -> more;
+        tmp -> less = TargetNode -> less;
+
         
+        if (TargetNode -> parent -> more == TargetNode)
+            TargetNode -> parent -> more = tmp;
+        else if (TargetNode -> parent -> less == TargetNode)
+            TargetNode -> parent -> less = tmp;
+        free(TargetNode);
+        
+    /*
+        tmp -> less = TargetNode -> less;
+        tmp -> parent = TargetNode -> parent;
+        TargetNode -> parent -> more = tmp;
+
+
+        TargetNode -> value = tmp -> value;
+        if (tmp -> more == NULL)
+            tmp -> parent -> less = NULL;
+        else tmp -> parent -> less = tmp -> more;
+
+        free(tmp);
+    */
     }
-    return root;
+    return desired;
 }
 
 
@@ -199,9 +223,10 @@ void Menu(){
 
 int main() {
     Node *root = malloc(sizeof(Node));
-    int arr[9] = {8, 3, 10, 1, 6, 4, 7, 14, 13};
+    int arr[] = {10, 5, 2, 1, 3, 4, 7, 6, 8, 9, 15, 12, 11, 13, 14, 17, 16, 18, 19};
     int n = sizeof(arr)/sizeof(int);
     FromArray (arr, n, root);
+
 
     int value;
     int choise = 1;
@@ -228,7 +253,9 @@ int main() {
             scanf("%d", &value);
             Node *desired = search(root, value);
             if (desired){
-                printf("%d deliting\n", desired -> value);
+                printf("%d deleting\n", desired -> value);
+                delete(desired);
+                printf("Deleted.\n");
             }
 
             
